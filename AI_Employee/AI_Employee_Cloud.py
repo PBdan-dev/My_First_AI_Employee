6350e05688fe4f37d7e4bbf8f4f05b16a4902ac3
Pierre-Baptiste Dandois <pb.dandois@gmail.com>
2026-05-28 00:49:50 +0200

    Description des changements effectués

--- AI_Employee/AI_Employee_Cloud.py
+++ AI_Employee/AI_Employee_Cloud.py
@@ -5,7 +5,7 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 class IAEmploye:
-    def __init__(self, api_key="AIzaSyAi62U27loBkZ3Kl7aUzhZ2A4xdfP88x3M"):
+    def __init__(self, api_key=""):
         # Initialisation de Gemini 1.5 Pro ou Flash
         # 'gemini-1.5-flash' est ultra rapide et souvent gratuit/très peu cher
         # 'gemini-1.5-pro' est le plus intelligent pour les tâches complexes
